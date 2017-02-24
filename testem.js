fb0043f9f6240cc6e83b82739fe7b3aeee7271e6
Jan Míšek <jan.misek@rclick.cz>
2017-02-24 22:30:07 +0100

    sauce labs automated tests

--- testem.js
+++ testem.js
@@ -3,11 +3,38 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+
+  launchers: {
+    SL_firefox_public: {
+      "exe": "ember",
+      "args": [
+        "sauce:launch",
+        "-b",
+        "firefox",
+        "--visibility",
+        "public",
+        "--attach",
+        "--no-connect",
+        "--url"
+      ],
+      "protocol": "browser"
+    },
+
+    SL_internet_explorer_public_11: {
+      "exe": "ember",
+      "args": [
+        "sauce:launch",
+        "-b",
+        "internet explorer",
+        "-v",
+        "11",
+        "--visibility",
+        "public",
+        "--attach",
+        "--no-connect",
+        "--url"
+      ],
+      "protocol": "browser"
+    }
+  }
 };
