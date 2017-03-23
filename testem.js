b729c8846b9d7c6bc9b564f352470aef90903879
Jan Míšek <jan.misek@rclick.cz>
2017-03-23 13:43:49 +0100

    Upgrade + tests based on souce labs

--- testem.js
+++ testem.js
@@ -3,38 +3,60 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-
-  launchers: {
-    SL_firefox_public: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "firefox",
-        "--visibility",
-        "public",
-        "--attach",
-        "--no-connect",
-        "--url"
-      ],
-      "protocol": "browser"
+  "timeout": 540,
+  "parallel": 1,
+  "launchers":
+  {
+    "SL_Chrome_Current": {
+      "command": "ember sauce:launch -b chrome -v 39 --no-ct -u <url>",
+      "protocol": "tap"
     },
-
-    SL_internet_explorer_public_11: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "internet explorer",
-        "-v",
-        "11",
-        "--visibility",
-        "public",
-        "--attach",
-        "--no-connect",
-        "--url"
-      ],
-      "protocol": "browser"
+    "SL_Chrome_Last": {
+      "command": "ember sauce:launch -b chrome -v 38 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_Firefox_Current": {
+      "command": "ember sauce:launch -b firefox -v 34 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_Firefox_Last": {
+      "command": "ember sauce:launch -b firefox -v 33 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_Safari_Current": {
+      "command": "ember sauce:launch -b safari -v 8 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_Safari_Last": {
+      "command": "ember sauce:launch -b safari -v 7 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_IE_11": {
+      "command": "ember sauce:launch -b 'internet explorer' -v 11 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_IE_10": {
+      "command": "ember sauce:launch -b 'internet explorer' -v 10 --no-ct -u <url>",
+      "protocol": "tap"
+    },
+    "SL_IE_9": {
+      "command": "ember sauce:launch -b 'internet explorer' -v 9 --no-ct -u <url>",
+      "protocol": "tap"
     }
   }
-};
+  ,
+  "launch_in_dev": [
+    "chromium"
+  ],
+  "launch_in_ci": [
+    "SL_Chrome_Current",
+    "SL_Chrome_Last",
+    "SL_Firefox_Current",
+    "SL_Firefox_Last",
+    "SL_Safari_Current",
+    "SL_Safari_Last",
+    "SL_IE_11",
+    "SL_IE_10",
+    "SL_IE_9"
+  ]
+}
